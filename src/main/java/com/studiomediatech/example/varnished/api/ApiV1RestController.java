958b125e261ba5d57f266321d3eefbadb9da55e9
Olle Törnström <toernstroem@synyx.de>
2020-12-04 23:03:02 +0100

    That was fun, REST api

--- src/main/java/com/studiomediatech/example/varnished/api/ApiV1RestController.java
+++ src/main/java/com/studiomediatech/example/varnished/api/ApiV1RestController.java
@@ -15,20 +15,20 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 /**
- * Yeah, always be naming your classes in an enterprisey manner. This is no joke. A nice and neat suffix-superfluous
- * naming schema never hurt anyone. On the contrary, it's been the back-bone of this Java toutin' industry for years
- * and years. Get over it.
+ * So, we're moving into the world of API wonders, this can now provide a developer-friendly index of some of the
+ * available resources under version 1 - you do speak HATEOAS, do you?
  */
 @RestController
-public class ApiRestController {
+public class ApiV1RestController {
 
-    static final Function<String, Link> API_LINK = rel -> linkTo(methodOn(ApiRestController.class).api()).withRel(rel);
+    static final Function<String, Link> API_V1_LINK = rel ->
+            linkTo(methodOn(ApiV1RestController.class).v1()).withRel(rel);
 
-    @GetMapping("/api")
-    public Map<String, Object> api() {
+    @GetMapping("/api/v1")
+    public Map<String, Object> v1() {
 
-        Link l1 = ApiRestController.API_LINK.apply("self");
-        Link l2 = ApiV1RestController.API_V1_LINK.apply("v1");
+        Link l1 = ApiRestController.API_LINK.apply("parent");
+        Link l2 = ApiV1RestController.API_V1_LINK.apply("self");
 
         return Collections.singletonMap("links", Arrays.asList(l1, l2));
     }
