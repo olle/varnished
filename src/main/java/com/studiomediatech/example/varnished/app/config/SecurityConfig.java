84ff620b93718aae05fb10af9ca59e2e8e83140c
Olle Törnström <toernstroem@synyx.de>
2020-12-05 00:48:13 +0100

    There is auth

--- src/main/java/com/studiomediatech/example/varnished/app/config/SecurityConfig.java
+++ src/main/java/com/studiomediatech/example/varnished/app/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.studiomediatech.example.varnished.web.config;
+package com.studiomediatech.example.varnished.app.config;
 
 import com.studiomediatech.example.varnished.app.VarnishedApplication;
 
@@ -7,15 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private Environment env;
 
-    public WebSecurityConfig(Environment env) {
+    public SecurityConfig(Environment env) {
 
         this.env = env;
     }
@@ -26,6 +27,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         if (env.acceptsProfiles(VarnishedApplication.DEVELOPMENT)) {
             auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
             auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+            auth.inMemoryAuthentication().withUser("dev").password("{noop}dev").roles("DEVELOPER");
         }
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/api/**").hasAnyRole("DEVELOPER");
+        http.authorizeRequests().antMatchers("/**").hasAnyRole("USER", "ADMIN");
+        http.formLogin().permitAll();
     }
 }
