b431eae86a1f2db78db9a8c43d409f138fa50af6
Olle Törnström <toernstroem@synyx.de>
2020-12-05 09:47:59 +0100

    Testing, it's all the rage these days, they say

--- src/test/java/com/studiomediatech/example/varnished/app/VarnishedApplicationIT.java
+++ src/test/java/com/studiomediatech/example/varnished/app/VarnishedApplicationIT.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
-class VarnishedApplicationTests {
+class VarnishedApplicationIT {
 
     @Test
     @DisplayName("the application context starts without failures")
