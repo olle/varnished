2e527d1aba6d047df196754a490e1576d4873fb6
Olle Törnström <toernstroem@synyx.de>
2021-12-17 14:50:05 +0100

    So we begin anew
    * Move old src into attic - starting clean and fresh with empty
      SB app.

---
+++ src/main/java/example/varnished/app/VarninshedApp.java
@@ -0,0 +1,14 @@
+package example.varnished.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class VarninshedApp {
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(VarninshedApp.class, args);
+    }
+
+}
