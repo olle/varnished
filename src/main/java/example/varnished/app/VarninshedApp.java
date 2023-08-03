5842f4b6c66bfcbfd9abc5e94ae9b53cfb7a9981
Olle Törnström <olle@studiomediatech.com>
2023-08-03 23:22:52 +0200

    There is a reason

--- src/main/java/example/varnished/app/VarninshedApp.java
+++ src/main/java/example/varnished/app/VarninshedApp.java
@@ -9,6 +9,12 @@ import org.springframework.context.annotation.Import;
 import example.varnished.infra.repo.RepoConfig;
 import example.varnished.web.WebConfig;
 
+/**
+ * The default profile, does not auto-configure JDBC, even though there are triggering assets on the classpath. This is
+ * so that we are able to run most unit and module testing with injected test dummies or mocks - as desirable. The
+ * Frobulating application will be a delight to develop and try out locally, with the option to run a full
+ * container-setup and the production-services using profiles.
+ */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class VarninshedApp {
 
