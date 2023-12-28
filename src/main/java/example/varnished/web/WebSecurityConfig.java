f8faaecc1f5897180137e02d6e7d2fb146fd729d
Olle Törnström <olle@studiomediatech.com>
2023-12-28 14:13:51 +0100

    Add security for management endpoints

--- src/main/java/example/varnished/web/WebSecurityConfig.java
+++ src/main/java/example/varnished/web/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package example.varnished.web;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,21 +23,28 @@ class WebSecurityConfig {
     @ConditionalOnMissingBean
     public UserDetailsService defaultUserDetailsService() {
         return new InMemoryUserDetailsManager( // NOSONAR
+                User.withUsername("admin").password("{noop}admin").roles("ADMIN").build(),
                 User.withUsername("user").password("{noop}user").roles("USER").build(),
                 User.withUsername("dev").password("{noop}dev").roles("DEVELOPER").build(),
                 User.withUsername("other").password("{noop}other").roles("OTHER").build());
     }
 
-    @Bean
+    @Bean("endpoint-security-chain")
+    public SecurityFilterChain endpointSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint());
+        http.authorizeHttpRequests(authz -> authz.anyRequest().hasRole("ADMIN")).formLogin(withDefaults())
+                .logout(withDefaults());
+        return http.build();
+    }
+
+    @Bean("web-mvc-security-chain")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.authorizeHttpRequests(authz()).formLogin(withDefaults()).logout(withDefaults());
-
         return http.build();
     }
 
     private Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authz() {
         return authz -> authz.requestMatchers("/**").hasAnyRole("DEVELOPER") // NOSONAR
-                .anyRequest().authenticated();
+                .anyRequest().denyAll();
     }
 }
